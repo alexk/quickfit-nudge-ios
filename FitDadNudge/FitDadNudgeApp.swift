d095f71276a43faec244076a7e2fac740f3d9d57
Alex Koff <abkoff@gmail.com>
2025-05-26 22:48:22 -0700

    Initial Commit

---
+++ FitDadNudge/FitDadNudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitDadNudgeApp.swift
+//  FitDadNudge
+//
+//  Created by Alex Koff on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct FitDadNudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitDadNudgeTests/FitDadNudgeTests.swift
@@ -0,0 +1,16 @@
+//
+//  FitDadNudgeTests.swift
+//  FitDadNudgeTests
+//
+//  Created by Alex Koff on 5/26/25.
+//
+
+import Testing
+
+struct FitDadNudgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
